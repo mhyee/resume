46a4be5278e7b044513f90858d8edfe207c86cbe
Ming-Ho Yee <ming-ho.yee@uwaterloo.ca>
2011-09-03 17:31:13 -0400

    Allow generation of resume-web, which doesn't contain the private contact information

--- generate.rb
+++ generate.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'yaml'
 require 'erb'
 require 'ostruct'
@@ -10,20 +12,28 @@ class ErbBinding < OpenStruct
 end
 
 # Load the resume YAML files
-# private.yml contains contact information I don't want posted
-resume = YAML::load( File.open('resume.yml') )
-private = YAML::load( File.open('private.yml') )
+resume = YAML::load( File.open("resume.yml") )
 
-# Merge the contact information
-resume["contact"] = resume["contact"].merge(private["contact"])
+if ARGV[0] == "web"
+  puts "Generating web version of resume..."
+  outputfile = "resume-web.html"
+else
+  puts "Generating normal version of resume..."
+  outputfile = "resume.html"
+
+  # Load and merge contact information (for full resume)
+  # private.yml contains contact information I don't want posted
+  private = YAML::load( File.open("private.yml") )
+  resume["contact"] = resume["contact"].merge(private["contact"])
+end
 
 # Load the ERB template (for now, only HTML)
-template = ERB.new( File.new('resume.html.erb').read, 0, "%<>" )
+template = ERB.new( File.new("resume.html.erb").read, 0, "%<>" )
 
 namespace = ErbBinding.new resume
 result = template.result namespace.send(:get_binding)
 
 # Write to output file
-File.open( 'resume.html', 'w' ) do |file|
+File.open( outputfile, "w" ) do |file|
   file.write result
 end
