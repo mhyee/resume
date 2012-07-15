52e8cb2a39c4879a340a4198855c7a2355a9c822
Ted Shieh <g161803@gmail.com>
2012-07-15 15:36:56 -0700

    add escaping for dollar signs

--- templates/escape_tex.rb
+++ templates/escape_tex.rb
@@ -13,6 +13,7 @@ def escape(input='')
     input.gsub!(/\[''\]/, "''")                 # Smart quotes (close)
     input.gsub!(/\[\+\]/, "$+$")                # Plus sign
     input.gsub!(/\[#\]/, "\\\\#")               # Number sign
+    input.gsub!(/\[\$\]/, "\\\\$")              # Dollar sign
     input.gsub!(/\[(.*)\]\((.*)\)/, "\\\\href{\\2}{\\1}")   # Link handling
   else
     input
