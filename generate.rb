8ebbc9e49d23e504ce747e400c59fc1fafdc2aff
Ted Shieh <g161803@gmail.com>
2012-07-15 17:04:00 -0700

    use OptionParser to read command-line arguments specifying the input file, the template, and whether or not to generate a web version

--- generate.rb
+++ generate.rb
@@ -1,8 +1,26 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
 require 'yaml'
 require 'erb'
 require 'ostruct'
+
+options = {}
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: generate.rb [options]"
+  options[:input] = 'resume.yml'
+  opts.on( '-i', '--input INPUT', 'input file' ) do |input|
+    options[:input] = input
+  end
+  options[:template] = 'resume.html.erb'
+  opts.on( '-t', '--template TEMPLATE', 'template file' ) do |template|
+    options[:template] = template
+  end
+  options[:web] = false
+  opts.on( '-w', '--web', 'create a web version omitting private information' ) do |w|
+    options[:web] = true
+  end
+end.parse!
 
 # Semi-hack to pass the correct bindings to ERB
 class ErbBinding < OpenStruct
@@ -11,28 +29,22 @@ class ErbBinding < OpenStruct
     end
 end
 
+# Does the resume file exist?
+abort("Error: #{options[:input]} is not present in this directory.  Use -i or --input to specify another input file.") unless File.exists?( options[:input] )
+
 # Load the resume YAML files
-resume = YAML::load( File.open("resume.yml") )
+resume = YAML::load( File.open(options[:input]) )
 
-if ARGV.size < 1
-  puts "usage: ./generate.rb <extension> [web]"
-  puts "\n"
-  puts "\t <extension> specifies the template, eg 'html' or 'tex'"
-  puts "\t use option [web] for web-version, default is full resume"
-  exit 0
-end
+# Does the template file exist?
+abort("Error: template #{options[:template]} is not present in the templates directory.  Use -t or --template to specify another template file.") unless File.exists?( 'templates/' + options[:template] )
 
-# Figure out which template we're using
-extension = ARGV[0].downcase
-template_file = "templates/resume." + extension + ".erb"
+input_basename, input_extension = options[:input].split('.')
+template_basename, template_extension1, template_extension2 = options[:template].split('.')
 
-# Does the template actually exist?
-abort("Error: template #{template_file} doesn't exist!") unless File.exists?( template_file )
-
-if ARGV[1] == "web"
-  output_file = "output/resume-web." + extension
+if options[:web]
+  output_file = input_basename + '-web.' + template_extension1
 else
-  output_file = "output/resume." + extension
+  output_file = input_basename + '.' + template_extension1
 
   # Load and merge contact information (for full resume)
   # private.yml contains contact information I don't want posted
@@ -41,12 +53,12 @@ else
 end
 
 # Load the escape function and run it on resume hash (if escape function exists)
-escape_defn = File.join( File.dirname(__FILE__), 'templates', '/escape_' + extension + '.rb' )
+escape_defn = File.join( File.dirname(__FILE__), 'templates', '/escape_' + template_extension1 + '.rb' )
 require escape_defn if File.exists?(escape_defn)
 escape(resume) if defined?(escape)
 
 # Load the ERB template
-template = ERB.new( File.new(template_file).read, 0, "<>" )
+template = ERB.new( File.new('templates/' + options[:template]).read, 0, "<>" )
 
 namespace = ErbBinding.new resume
 result = template.result namespace.send(:get_binding)
@@ -55,7 +67,7 @@ result = template.result namespace.send(:get_binding)
 Dir.mkdir("output") unless File.exists?("output") && File.directory?("output")
 
 # Write to output file
-File.open( output_file, "w" ) do |file|
+File.open( 'output/' + output_file, "w" ) do |file|
   file.write result
 end
 puts "Created #{output_file}"
