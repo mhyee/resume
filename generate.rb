621ec8e86409d9cf0f19f6f3ea7be21d37ad94c1
Ming-Ho Yee <mh@mhyee.com>
2023-10-17 23:24:44 -0400

    Update generate.rb script

--- generate.rb
+++ generate.rb
@@ -34,16 +34,16 @@ class ErbBinding < OpenStruct
 end
 
 # Does the resume file exist?
-abort("Error: #{options[:input]} is not present in this directory.  Use -i or --input to specify another input file.") unless File.exists?( options[:input] )
+abort("Error: #{options[:input]} is not present in this directory.  Use -i or --input to specify another input file.") unless File.exist?( options[:input] )
 
 # Load the resume YAML files
 resume = YAML::load( File.open(options[:input]) )
 
 # Does the private input file exist?
-abort("Error: #{options[:private]} is not present in this directory.  Use -p or --private to specify another private input file.") unless File.exists?( options[:private] ) or options[:web]
+abort("Error: #{options[:private]} is not present in this directory.  Use -p or --private to specify another private input file.") unless File.exist?( options[:private] ) or options[:web]
 
 # Does the template file exist?
-abort("Error: template #{options[:template]} is not present in the templates directory.  Use -t or --template to specify another template file.") unless File.exists?( 'templates/' + options[:template] )
+abort("Error: template #{options[:template]} is not present in the templates directory.  Use -t or --template to specify another template file.") unless File.exist?( 'templates/' + options[:template] )
 
 input_basename, input_extension = options[:input].split('.')
 template_basename, template_extension1, template_extension2 = options[:template].split('.')
@@ -61,17 +61,17 @@ end
 
 # Load the escape function and run it on resume hash (if escape function exists)
 escape_defn = File.join( File.dirname(__FILE__), 'templates', '/escape_' + template_extension1 + '.rb' )
-require escape_defn if File.exists?(escape_defn)
+require escape_defn if File.exist?(escape_defn)
 escape(resume) if defined?(escape)
 
 # Load the ERB template
-template = ERB.new( File.new('templates/' + options[:template]).read, 0, "<>" )
+template = ERB.new( File.new('templates/' + options[:template]).read, trim_mode: "<>" )
 
 namespace = ErbBinding.new resume
 result = template.result namespace.send(:get_binding)
 
 # Create 'output' directory if it doesn't exist
-Dir.mkdir("output") unless File.exists?("output") && File.directory?("output")
+Dir.mkdir("output") unless File.exist?("output") && File.directory?("output")
 
 # Write to output file
 File.open( 'output/' + output_file, "w" ) do |file|
