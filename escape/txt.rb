af1a6b26be24cf72e55070ba7e6f0b97dc5042c3
Ming-Ho Yee <mh@mhyee.com>
2024-01-31 20:41:09 -0500

    [txt] Improve word wrap

--- escape/txt.rb
+++ escape/txt.rb
@@ -22,3 +22,16 @@ def escape(input='')
     input
   end
 end
+
+def wrap(text, indent=0)
+  spaces = " " * indent
+
+  # The regex matches up to 74 characters, then whitespace or end of line.
+  # The whitespace or end of line is replaced by a linebreak, followed by
+  # indentation.
+  text
+    .gsub(/\]\(/, "] (")                        # allow breaks in URL tags
+    .gsub(/(.{1,74})(\s+|$)/, "\\1\n#{spaces}") # insert a linebreak after 74 columns
+    .gsub(/\] \(/, "](")                        # remove extraneous space in URL tags
+    .strip                                      # remove extra whitespace
+end
